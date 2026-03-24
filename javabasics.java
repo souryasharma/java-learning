3fd6512706f33c9439a5dcd4d7569584661d2529
souryasharma <kumarsourya120@gmail.com>
2026-03-25 01:22:38 +0530

    this is my first output program for java

---
+++ javabasics.java
@@ -0,0 +1,9 @@
+public class javabasics{
+    public static void main(String[] args) {
+        System.out.print("hello sourya\n");
+         System.out.print("hello sourya\n");
+        System.out.print("hello sourya\n");
+       
+    }
+}
+//boilerplate code
